27c66ecf600edbcb1bdb630ee18a168f447c4dd8
Iftekar Hossain <iftekar.hossain123@gmail.com>
2026-07-20 14:52:04 -0400

    Loading the data

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def ask_ollama(prompt: str, model: str = "qwen3:8b") -> str:
         "prompt": prompt,
         "stream": False,
     }
+
     request = urllib.request.Request(
         "http://localhost:11434/api/generate",
         data=json.dumps(payload).encode("utf-8"),
@@ -39,6 +40,31 @@ def ask_ollama(prompt: str, model: str = "qwen3:8b") -> str:
 
     return result["response"].strip()
 
+def load_documents_from_directory(directory_path): 
+    print("====== Loading document from directory ======")
+    documents = []
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".pdf"):
+            with open(
+                os.path.join(directory_path, filename), "r", encoding="utf-8"
+            ) as file:
+                documents.append({"id": filename, "text": file.read()})
+    return documents
 
-response = ask_ollama("How much points are offered for this card?")
-print(response)
+def split_text(text, chunk_size=1000, chunk_overlap=20): 
+    chunks = []
+    start = 0
+
+    while start < len(text): 
+        end = start + chunk_size
+        chunks.append(text[start:end])
+        start = end - chunk_overlap
+    return chunks
+
+
+directory_path = "./card_details"
+documents = load_documents_from_directory(directory_path)
+print(f"Loaded {len(documents)} documents")
+
+# response = ask_ollama("How much points are offered for this card?")
+# print(response)
